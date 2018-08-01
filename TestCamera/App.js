b8354251854c58f92378ddd8903cac851d596cff
Farid Lkhaldouni <flkhaldouni@coopbelvedere.com>
2018-08-01 17:42:22 -0400

    fix camera + upgrade gradle + upgarde distribution + link module

--- TestCamera/App.js
+++ TestCamera/App.js
@@ -7,35 +7,64 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Modal, TouchableOpacity} from 'react-native';
+import { RNCamera } from 'react-native-camera';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        cameraVisible: false,
+      }
+  }
+
+  toggleCamera() {
+    // console.log('openCamera');
+    this.setState({cameraVisible: true});
+  }
+
+  closeCamera() {
+    this.setState({cameraVisible: false});
+  }
+
+  takePicture() {
+  this.camera.capture()
+  .then(function(data) {
+    console.log(data.path);
+  })
+ .catch(err => console.error("error: " + err));
+}
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <RNCamera
+            ref={ref => {
+              this.camera = ref;
+            }}
+            style = {styles.preview}
+            type={RNCamera.Constants.Type.back}
+            flashMode={RNCamera.Constants.FlashMode.on}
+            permissionDialogTitle={'Permission to use camera'}
+            permissionDialogMessage={'We need your permission to use your camera phone'}
+        />
+        <View style={{flexDirection: 'row', justifyContent: 'center',}}>
+          <TouchableOpacity
+              onPress={() => this.takePicture.bind(this)}
+              style = {styles.capture}
+          >
+              <Text style={{fontSize: 14}}> SNAP </Text>
+          </TouchableOpacity>
+          </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
@@ -46,4 +75,23 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black'
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20
+  }
 });
